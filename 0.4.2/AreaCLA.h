0b1e9ac0e62f73ee87a26f2b8a66bbaaeadca561
Hrodvitnir <alexshakirov74@gmail.com>
2016-11-27 22:36:40 +0500

    0.4.2

---
+++ 0.4.2/AreaCLA.h
@@ -0,0 +1,47 @@
+#pragma once
+                    
+#include <vector>
+#include <Windows.h>
+#include <random>  
+#include "Prim.h"
+
+#define MIN_COUNT_OF_CRT 8
+#define COUNT_OF_CRT 64
+
+#define UNI unsigned short int
+
+class Prim;
+class CreationC;
+
+class AreaCLA
+{
+    bool _countOfCtrnsChenged;
+	Prim*** _map;
+	int _heigh;//x
+	int _width;//y
+	UNI _countOfFood;
+	UNI _countOfPoison;   
+    UNI _FPCount;
+    
+    vector <CreationC*> _crtns;        
+
+	void _craftAreaCLA();
+    void _cicle();
+    void _refresh();
+
+public:
+	AreaCLA(int heigh);
+	~AreaCLA();
+	void Print();
+	void SetFood(COORD coord);
+	void SetPoison(COORD coord);
+	void SetWall(COORD coord);
+	void SetVoid(COORD coord);
+	void Start();
+
+	void minFood();//минус еда
+	void minPoison();//минус яд
+    void delCrt(CreationC*);// Удалить существо из вектора
+	Prim*** GetArea();
+};
+
---
+++ 0.4.2/Исходный код.cpp
@@ -0,0 +1,8 @@
+#include "AreaCLA.h"
+
+int main()
+{
+    AreaCLA ar(10);
+    system("pause");
+    return 0;
+}
