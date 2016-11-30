f8900dccfeee444017a5ccb71c15b509d55c2846
Hrodvitnir <alexshakirov74@gmail.com>
2016-11-30 18:36:45 +0500

    0.5.2
    Все готово

---
+++ 0.5.2/AreaCLA.h
@@ -0,0 +1,54 @@
+#pragma once
+                    
+#include <vector>
+#include <Windows.h>
+#include <random>  
+#include "Prim.h"
+
+#define MIN_COUNT_OF_CRT 4
+#define COUNT_OF_CRT 20
+
+#define UNI unsigned short int
+using namespace std;
+
+class Prim;
+class CreationC;
+
+class AreaCLA
+{
+    bool _countOfCtrnsChenged;
+	Prim*** _map;
+	int _heigh;//x
+	int _width;//y
+	UNI _countOfFood;
+	UNI _countOfPoison;   
+    UNI _FPCount;
+    
+    CreationC** _crtns;        
+    int _crtnsCount;
+    int _maxCrtnsCount;
+    int _minCrtnsCount;
+
+
+	void _craftAreaCLA();
+    void _cicle();
+    void _refresh();
+    void _Reborn();
+    void _chengeArea();
+
+public:
+	AreaCLA(int heigh);
+	~AreaCLA();
+	void Print();
+	void SetFood(COORD coord);
+	void SetPoison(COORD coord);
+	void SetWall(COORD coord);
+	void SetVoid(COORD coord);
+	void Start();
+
+	void minFood();//минус еда
+	void minPoison();//минус яд
+    void delCrt(int, int, int);// Удалить существо из вектора
+	Prim*** GetArea();
+};
+
---
+++ 0.5.2/Prim.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "AreaCLA.h"
+#include <iostream>
+#include <windows.h> 
+#include <vector>
+#include <random>
+#include <boost\signal.hpp>
+#include <boost\bind.hpp> 
+
+#define WallDamage 5
+#define PoisonDamage 15
+#define CrtDamage 3
+#define FoodPlus 10
+#define UNI unsigned short int
+
+using namespace std;
+
+enum Type {Void, Food, Poison, Wall, Crt};
+
+class AreaCLA;
+
+struct Color
+{
+    int R;
+    int G;
+    int B;
+    void operator=(Color& clr);
+};
+
+class Prim
+{
+protected:
+    Type _type;
+	int _x;
+	int _y;
+    int _healthPointChange;
+    Color _color;
+
+public:
+	Type GetType();
+	void SetType(Type);
+	int GetX();
+	int GetY();
+	void SetX(int);
+	void SetY(int);
+	Prim();
+	Prim(Type);
+	~Prim();
+    int getHPC();
+    void setHPC(int);
+    Color& getColor();
+};
+
+class CreationC : public Prim//CRT
+{
+    enum direction {lU, up, uR, right, rD, dowm, dL, left};
+
+    AreaCLA* _world;
+    direction _head;
+    short int _hp;
+    vector<UNI> _commandList; //[1; 8] - ходит [9; 16] - хавать 
+    UNI _itForCommand;
+    int _countOfStep;
+    Prim*** _area;   
+    
+    boost::signal <void()> _eatingFood;
+    boost::signal <void()> _eatingPoison;
+
+    bool _IsClose(int X, int Y);
+    //.......................................
+    void _Step(UNI);
+    bool _See(UNI);
+    void _Roll(UNI); 
+    void _AddSlots();
+    void _Eat(UNI);
+public:            
+    void Mutation();
+    bool IsAlive();
+    void Execute();
+	CreationC(vector<UNI>& gens, AreaCLA*, int x, int y);
+    CreationC();
+    vector<UNI>& GetCommandList();
+	~CreationC();
+};
