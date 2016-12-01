769d5c8a7af66d6dba29ba1ecf1cf4e9e6f232f3
Hrodvitnir <alexshakirov74@gmail.com>
2016-12-01 19:04:41 +0500

    Alpha 0.1
    Сделал так чтобы боты не могли зависать

---
+++ Alpha 0.1/log.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define LOGS  
+
+#ifdef LOGS
+
+#define LOG(str)      \
+    out << str << endl; \
+
+#define WLOG(str)  \
+    _world->out << str << endl;  \
+
+#endif
+#ifndef LOGS
+
+#define LOG(str)
+#define WLOG(str)
+
+
+#endif
